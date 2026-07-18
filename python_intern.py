bfef4b0d701f119bc5725f26a4eb7d90123fb164
Esha Samar <300114312+eshasamar5-blip@users.noreply.github.com>
2026-07-18 18:19:05 +0500

    Added Python calculator program

---
+++ python_intern.py
@@ -0,0 +1,37 @@
+
+import math
+
+while True:
+    operation = input("CHOOSE  (+,-,/,*,sin,cos) or type  'end'  to exit:")
+    if operation=="end":
+        print ("Shutting down...")
+        break
+    if operation in ["=","-","*","/"]:
+        a=float(input("ENTER FIRST NUMBER:"))
+        b=float(input("ENTER SECOND NUMBER:"))
+        if operation =="+":
+            print ("Result", a+b)
+        elif operation =="-":
+            print ("result:", a-b)
+        elif operation =="*":
+            print ("result:", a*b)
+        elif operation =="/":
+            if b==0:
+                print ("SYNTAX Error")
+            else:
+                print ("result:", a/b)
+
+    elif operation == "sin":
+        angle=float(input("ENTER ANGLE"))
+        unit=input("DEGREE OR RADIANS d/r :")
+        if unit=="d":
+            angle=math.radians(angle)
+        print ("Result:", math.sin(angle))
+    elif operation == "cos":
+        angle = float (input("ENTER ANGLE:"))
+        unit=input("DEGREE OR RADIANS d/r:")
+        if unit==" d":
+            angle=math.radians(angle)
+        print ("Result:", math.cos(angle))
+    else:
+        print ("INVALID operation")
